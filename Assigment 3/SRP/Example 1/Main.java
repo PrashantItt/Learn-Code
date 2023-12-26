7049f8ca829905aebd854b111fb03b716bfab8e4
Prashant Kumar <prashantchapaith@gmail.com>
2023-12-26 10:11:23 +0530

    Assignment 3

---
+++ Assigment 3/SRP/Example 1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle(5.0, 3.0);
+
+        RectangleAreaPrint rectangleAreaPrint = new RectangleAreaPrint();
+
+        rectangleAreaPrint.printArea(rectangle);
+    }
+}
